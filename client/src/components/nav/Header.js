3c88521cf0dacb555e54699b82ed3aba038ad299
Royal <thaparoyal17@gmail.com>
2021-07-11 09:02:25 +0545

    user logout

--- client/src/components/nav/Header.js
+++ client/src/components/nav/Header.js
@@ -6,16 +6,32 @@ import {
   SettingOutlined,
   UserOutlined,
   UserAddOutlined,
+  LogoutOutlined,
 } from "@ant-design/icons";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
+
+import firebase from "firebase";
 
 const { SubMenu, Item } = Menu;
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
+  let dispatch = useDispatch();
+  let history = useHistory();
 
   const handleClick = (e) => {
     // console.log(e.key);
     setCurrent(e.key);
+  };
+
+  const logout = () => {
+    firebase.auth().signOut();
+    dispatch({
+      type: "LOGOUT",
+      payload: null,
+    });
+    history.push("/login");
   };
 
   return (
@@ -27,6 +43,9 @@ const Header = () => {
       <SubMenu key="submenu" icon={<SettingOutlined />} title="Username">
         <Item key="setting:1">Option 1</Item>
         <Item key="setting:2">Option 2</Item>
+        <Item icon={<LogoutOutlined />} onClick={logout}>
+          Logout
+        </Item>
       </SubMenu>
 
       <Item key="login" icon={<UserOutlined />} className="ml-auto">
