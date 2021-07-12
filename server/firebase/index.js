d8a551ddd0e6bb077e488fe76be2802f55c613be
Royal <thaparoyal17@gmail.com>
2021-07-12 22:10:48 +0545

    firebase admin

---
+++ server/firebase/index.js
@@ -0,0 +1,7 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("../config/fbServiceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
