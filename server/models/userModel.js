31386751e79b9d0aaa8619d60097acffee756327
Royal <thaparoyal17@gmail.com>
2021-07-12 10:14:19 +0545

    user Schema : Node MongoDB API (Backend)

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const userSchema = new mongoose.Schema(
+  {
+    name: String,
+    email: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    role: {
+      type: String,
+      default: "subscriber",
+    },
+    cart: {
+      type: Array,
+      default: [],
+    },
+    address: String,
+    //   wishlist: [
+    //     {
+    //       type: ObjectId,
+    //       ref: "Product",
+    //     },
+    //   ],
+  },
+  { timeStamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
