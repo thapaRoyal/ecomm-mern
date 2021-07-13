2a503e3fcd9c5456c705d2d484e31c71afd6a271
Royal <thaparoyal17@gmail.com>
2021-07-13 21:47:08 +0545

    sidebar nav for user :User admin and protected routes

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -1,9 +1,13 @@
 import React from "react";
+import UserNav from "../../components/nav/UserNav";
 
 const History = () => {
   return (
     <div className="container-fluid">
       <div className="row">
+        <div className="col md-2">
+          <UserNav />
+        </div>
         <div className="col">User history page</div>
       </div>
     </div>
