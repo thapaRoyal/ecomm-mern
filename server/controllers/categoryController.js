18a1e99c2d31c13c9a6807b7f78ee3715c70c134
Royal <thaparoyal17@gmail.com>
2021-07-14 09:14:55 +0545

    Category List Read Update Delete  :Categories CRUD

--- server/controllers/categoryController.js
+++ server/controllers/categoryController.js
@@ -12,14 +12,30 @@ exports.create = async (req, res) => {
   }
 };
 exports.list = async (req, res) => {
-  //
+  res.json(await Category.find({}).sort({ createdAt: -1 }).exec());
 };
 exports.read = async (req, res) => {
-  //
+  let category = await Category.findOne({ slug: req.params.slug }).exec();
+  res.json(category);
 };
 exports.update = async (req, res) => {
-  //
+  const { name } = req.body;
+  try {
+    let updated = await Category.findOneAndUpdate(
+      { slug: req.params.slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json(updated);
+  } catch (err) {
+    res.status(400).send("category update failed");
+  }
 };
 exports.remove = async (req, res) => {
-  //
+  try {
+    const deleted = await Category.findOneAndDelete({ slug: req.params.slug });
+    res.json(deleted);
+  } catch (error) {
+    res.status(400).send("category delete failed");
+  }
 };
--- server/routes/category.js
+++ server/routes/category.js
@@ -17,7 +17,7 @@ const {
 // ROUTES
 router.post("/category", authCheck, adminCheck, create);
 router.get("/categories", list);
-router.get("/category/:slug", authCheck, adminCheck, read);
+router.get("/category/:slug", read);
 router.put("/category/:slug", authCheck, adminCheck, update);
 router.delete("/category/:slug", authCheck, adminCheck, remove);
 
