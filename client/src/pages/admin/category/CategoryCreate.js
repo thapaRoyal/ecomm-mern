22bad859eca9aa65a569eac6807cf4e0550f12a6
Royal <thaparoyal17@gmail.com>
2021-07-14 20:57:19 +0545

    Category Create and List :Categories CRUD

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,14 +1,77 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import {
+  createCategory,
+  getCategories,
+  removeCategory,
+} from "../../../functions/category";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const CategoryCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createCategory({ name }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created`);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+        setName("");
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  const categoryForm = () => {
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label>Name</label>
+          <input
+            type="text"
+            className="form-control"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            autoFocus
+            required
+          />
+          <br />
+          <button className="btn btn-outline-primary">Create</button>
+        </div>
+      </form>
+    );
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
-        <div className="col md-2">
+        <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category create page</div>
+        <div className="col">
+          {loading ? <LoadingOutlined /> : <h4>Create Category</h4>}
+          {categoryForm()}
+          <hr />
+          {JSON.stringify(categories)}
+        </div>
       </div>
     </div>
   );
