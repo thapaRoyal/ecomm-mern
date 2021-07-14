b3f1313eb6d696db5fe4bb86d71bf7d4ba2e5af0
Royal <thaparoyal17@gmail.com>
2021-07-14 08:24:25 +0545

    Category routes :Categories CRUD

---
+++ server/controllers/categoryController.js
@@ -0,0 +1,17 @@
+const Category = require("../models/categoryModel");
+
+exports.create = (req, res) => {
+  //
+};
+exports.list = (req, res) => {
+  //
+};
+exports.read = (req, res) => {
+  //
+};
+exports.update = (req, res) => {
+  //
+};
+exports.remove = (req, res) => {
+  //
+};
---
+++ server/routes/category.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const router = express.Router();
+
+// middlewares
+const { authCheck, adminCheck } = require("../middlewares/authMiddleware");
+
+// controllers
+const {
+  create,
+  read,
+  update,
+  remove,
+  list,
+} = require("../controllers/category");
+
+// ROUTES
+router.post("/category", authCheck, adminCheck, create);
+router.get("/categories", list);
+router.get("/category/:slug", authCheck, adminCheck, read);
+router.put("/category/:slug", authCheck, adminCheck, update);
+router.delete("/category/:slug", authCheck, adminCheck, remove);
+
+module.exports = router;
