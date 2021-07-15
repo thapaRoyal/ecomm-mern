b225c46eeda2502e89baa96ac29d35a3d638e319
Royal <thaparoyal17@gmail.com>
2021-07-15 09:54:10 +0545

    Product model:Creating products with categories and sub categories (Advance)

---
+++ server/models/productModel.js
@@ -0,0 +1,77 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: true,
+      maxlength: 32,
+      text: true,
+    },
+
+    slug: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      index: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      maxlength: 2000,
+      text: true,
+    },
+
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      maxlength: 32,
+    },
+
+    category: {
+      type: ObjectId,
+      ref: "Category",
+    },
+
+    subs: [
+      {
+        type: ObjectId,
+        ref: "Sub",
+      },
+    ],
+    quantity: Number,
+    sold: {
+      type: Number,
+      default: 0,
+    },
+    images: {
+      type: Array,
+    },
+    shipping: {
+      type: String,
+      enum: ["Yes", "No"],
+    },
+    color: {
+      type: String,
+      enum: ["Black", "White", "Brown", "Silver", "Blue"],
+    },
+
+    brand: {
+      type: String,
+      enum: ["Apple", "Lenovo", "Samsung", "Microsoft", "Asus"],
+    },
+    // ratings: [
+    //   {
+    //     star: Number,
+    //     postedBy: { type: ObjectId },
+    //   },
+    // ],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
