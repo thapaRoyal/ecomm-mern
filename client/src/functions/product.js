463a5ce9e7b90efc0c5b7592aa873c278243bd67
thapaRoyal <thaparoyal17@gmail.com>
2021-07-16 23:08:00 +0545

    code fix

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -1,13 +1,11 @@
 import axios from "axios";
 
-export const createProduct = async (product, authtoken) => {
-  return await axios.post(`${process.env.REACT_APP_API}/product`, product, {
+export const createProduct = async (product, authtoken) =>
+  await axios.post(`${process.env.REACT_APP_API}/product`, product, {
     headers: {
       authtoken,
     },
   });
-};
 
-export const getProductsByCount = async (count) => {
+export const getProductsByCount = async (count) =>
   await axios.get(`${process.env.REACT_APP_API}/products/${count}`);
-};
