90b0b614dc1b264a077fc49a8fbb2d0fe466b2c7
Royal <thaparoyal17@gmail.com>
2021-07-16 13:39:26 +0545

    File Resize in React : multiple image uploads

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,9 +1,32 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   const fileUploadAndResize = (e) => {
     console.log(e.target.files);
     // resize
+    let files = e.target.files;
+
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          "base64"
+        );
+      }
+    }
 
     // send back to server to upload to cloudinary
 
