b9ee9c488a6d50632771157962ffdf7eaf28928c
thapaRoyal <thaparoyal17@gmail.com>
2021-07-17 15:52:19 +0545

    Product Card Component :displaying products pagination and carousel

---
+++ client/src/components/cards/ProductCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ProductCard = ({ product }) => {
+  return (
+    <div>
+      <p>{product.title}</p>
+    </div>
+  );
+};
+
+export default ProductCard;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,6 @@
+import { LoadingOutlined } from "@ant-design/icons";
 import React, { useEffect, useState } from "react";
+import ProductCard from "../components/cards/ProductCard";
 import { getProductsByCount } from "../functions/product";
 
 const Home = () => {
@@ -7,19 +9,32 @@ const Home = () => {
 
   useEffect(() => {
     loadAllProducts();
-  });
+  }, []);
 
   const loadAllProducts = () => {
+    setLoading(true);
     getProductsByCount(3).then((res) => {
+      console.log(res.data);
       setProducts(res.data);
+      setLoading(false);
     });
   };
 
   return (
-    <div>
-      <p>react home</p>
-      {JSON.stringify(products)}
-    </div>
+    <>
+      <div className="jumbotron">
+        {loading ? <LoadingOutlined className="h1" /> : <h4>All Products</h4>}
+      </div>
+      <div className="container">
+        <div className="row">
+          {products.map((product) => (
+            <div className="col-md-4" key={product._id}>
+              <ProductCard product={product} />
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
