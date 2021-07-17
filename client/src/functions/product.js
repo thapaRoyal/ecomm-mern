9be181a536dc1c098e92812ea600a08493211a05
thapaRoyal <thaparoyal17@gmail.com>
2021-07-17 12:08:44 +0545

    Get Product on Update Page : Update and delete products(Advance)

--- client/src/functions/product.js
+++ client/src/functions/product.js
@@ -16,3 +16,6 @@ export const removeProduct = async (slug, authtoken) =>
       authtoken,
     },
   });
+
+export const getProduct = async (slug) =>
+  await axios.get(`${process.env.REACT_APP_API}/product/${slug}`);
--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -2,18 +2,47 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import { LoadingOutlined } from "@ant-design/icons";
 
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "Asus", "Dell"],
+  color: "",
+  brand: "",
+};
+
 const ProductUpdate = ({ match }) => {
+  // state
+  const [values, setValues] = useState(initialState);
+
   // redux
   const { user } = useSelector((state) => ({ ...state }));
 
   // router
   const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then((p) => {
+      console.log("SINGLE PRODUCT", p);
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   return (
     <div className="container-fluid">
@@ -23,7 +52,7 @@ const ProductUpdate = ({ match }) => {
         </div>
         <div className="col-md-10">
           <h4>Update Product</h4>
-          {slug}
+          {JSON.stringify(values)}
           <hr />
         </div>
       </div>
