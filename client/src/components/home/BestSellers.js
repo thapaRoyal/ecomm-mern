961eec76f49a9d87a8dbddf29e8fee8019b1d7b0
thapaRoyal <thaparoyal17@gmail.com>
2021-07-17 18:57:11 +0545

    best sellers Pagination :displaying products pagination and carousel

--- client/src/components/home/BestSellers.js
+++ client/src/components/home/BestSellers.js
@@ -1,21 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { getProducts } from "../../functions/product";
+import { getProducts, getProductsCount } from "../../functions/product";
 import LoadingCard from "../cards/LoadingCard";
 import ProductCard from "../cards/ProductCard";
+import { Pagination } from "antd";
 
 const BestSellers = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [productsCount, setProductsCount] = useState(0);
 
   useEffect(() => {
     loadAllProducts();
+  }, [page]);
+
+  useEffect(() => {
+    getProductsCount().then((res) => setProductsCount(res.data));
   }, []);
 
   const loadAllProducts = () => {
     setLoading(true);
 
     // sort , order, limit
-    getProducts("sold", "desc", 3).then((res) => {
+    getProducts("sold", "desc", page).then((res) => {
       console.log(res.data);
       setProducts(res.data);
       setLoading(false);
@@ -36,6 +43,15 @@ const BestSellers = () => {
             ))}
           </div>
         )}
+      </div>
+      <div className="row">
+        <nav className="col-md-4 offset-md-4 text-center pt-5 p-3">
+          <Pagination
+            current={page}
+            total={(productsCount / 3) * 10}
+            onChange={(value) => setPage(value)}
+          />
+        </nav>
       </div>
     </>
   );
