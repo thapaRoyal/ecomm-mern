26b87357069adbe133b8a6aaee69aab49980ced5
thapaRoyal <thaparoyal17@gmail.com>
2021-07-17 11:10:51 +0545

    Product delete endpoint: Update and delete products(Advance)

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -25,3 +25,15 @@ exports.listAll = async (req, res) => {
     .exec();
   res.json(products);
 };
+
+exports.remove = async (req, res) => {
+  try {
+    const deleted = await Product.findOneAndRemove({
+      slug: req.params.slug,
+    }).exec();
+    res.json(deleted);
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Product delete failed");
+  }
+};
