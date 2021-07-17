a50137fd79484f00baa48d7890e980659346e06b
thapaRoyal <thaparoyal17@gmail.com>
2021-07-17 15:37:36 +0545

    Fetch Products in Home Page :displaying products pagination and carousel

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductsByCount } from "../functions/product";
 
-const Home = () => (
-  <div>
-    <p>react home</p>
-  </div>
-);
+const Home = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  });
+
+  const loadAllProducts = () => {
+    getProductsByCount(3).then((res) => {
+      setProducts(res.data);
+    });
+  };
+
+  return (
+    <div>
+      <p>react home</p>
+      {JSON.stringify(products)}
+    </div>
+  );
+};
 
 export default Home;
