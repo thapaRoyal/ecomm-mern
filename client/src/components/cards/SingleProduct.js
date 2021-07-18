ad6bef8ecb4a2c65690819bfca27214288b66a74
thapaRoyal <thaparoyal17@gmail.com>
2021-07-18 12:52:51 +0545

    Show Average Rating :Star Rating System

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -8,6 +8,7 @@ import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
 import StarRating from "react-star-ratings";
 import RatingModal from "../modal/ratingModal";
+import { showAverage } from "../../functions/rating";
 
 const { TabPane } = Tabs;
 
@@ -39,6 +40,10 @@ const SingleProduct = ({ product, onStarClick, star }) => {
 
       <div className="col-md-5">
         <h1 className="bg-info p-3">{title}</h1>
+
+        {product && product.ratings && product.ratings.length > 0
+          ? showAverage(product)
+          : "No rating yet"}
 
         <Card
           actions={[
