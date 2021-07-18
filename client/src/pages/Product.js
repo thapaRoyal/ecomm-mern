fa8a1a41272124fbc88d7e65a76a7f26439b0976
thapaRoyal <thaparoyal17@gmail.com>
2021-07-18 12:22:39 +0545

    Show Users Existing Star Rating :Star Rating System

--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -15,6 +15,15 @@ const Product = ({ match }) => {
     loadSingleProduct();
   }, [slug]);
 
+  useEffect(() => {
+    if (product.ratings && user) {
+      let existingRatingObject = product.ratings.find(
+        (ele) => ele.postedBy.toString() === user._id.toString()
+      );
+      existingRatingObject && setStar(existingRatingObject.star); //current user star
+    }
+  });
+
   const loadSingleProduct = () =>
     getProduct(slug).then((res) => setProduct(res.data));
 
