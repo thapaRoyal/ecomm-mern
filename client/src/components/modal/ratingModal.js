4d54638eb740ce9d5f9becae583e02cf410b0dad
thapaRoyal <thaparoyal17@gmail.com>
2021-07-18 10:45:33 +0545

    login to leave rating :Star Rating System

--- client/src/components/modal/ratingModal.js
+++ client/src/components/modal/ratingModal.js
@@ -3,15 +3,26 @@ import { Modal, Button } from "antd";
 import { StarOutlined } from "@ant-design/icons";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 const RatingModal = ({ children }) => {
   // redux
   const { user } = useSelector((state) => ({ ...state }));
   const [modalVisible, setModalVisible] = useState(false);
 
+  let history = useHistory();
+
+  const handleModal = () => {
+    if (user && user.token) {
+      setModalVisible(true);
+    } else {
+      history.push("/login");
+    }
+  };
+
   return (
     <>
-      <div onClick={() => setModalVisible(true)}>
+      <div onClick={handleModal}>
         <StarOutlined className="text-danger" /> <br />{" "}
         {user ? "Leave rating" : " Login to leave rating"}
       </div>
