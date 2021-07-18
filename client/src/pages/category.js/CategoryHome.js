f4df0a35869538171b8ebf7c71b5b6bdb9c6dfd6
thapaRoyal <thaparoyal17@gmail.com>
2021-07-18 16:22:15 +0545

    Category Page with Products

--- client/src/pages/category.js/CategoryHome.js
+++ client/src/pages/category.js/CategoryHome.js
@@ -2,22 +2,46 @@ import React, { useState, useEffect } from "react";
 import { getCategories, getCategory } from "../../functions/category";
 import { Link } from "react-router-dom";
 import ProductCard from "../../components/cards/ProductCard";
+import { LoadingOutlined } from "@ant-design/icons";
 
 const CategoryHome = ({ match }) => {
   const [category, setCategory] = useState({});
-  const [product, setProduct] = useState([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
   const { slug } = match.params;
 
   useEffect(() => {
     setLoading(true);
     getCategory(slug).then((c) => {
-      console.log(JSON.stringify(c.data, null,4));
-      setCategory(c.data);
+      console.log(JSON.stringify(c.data, null, 4));
+      setCategory(c.data.category);
+      setProducts(c.data.products);
+      setLoading(false);
     });
   }, []);
 
-  return <div>{match.params.slug}</div>;
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          {loading ? (
+            <LoadingOutlined className="text-center p-3 mt-5 mb-5 display-4 jumbotron" />
+          ) : (
+            <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+              {products.length} Products in "{category.name}" Category
+            </h4>
+          )}
+        </div>
+      </div>
+      <div className="row">
+        {products.map((p) => (
+          <div className="col" key={p._id}>
+            <ProductCard product={p} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default CategoryHome;
