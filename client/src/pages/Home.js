fa34149387b95b7aab873efc0cbeb6d9ee89b960
thapaRoyal <thaparoyal17@gmail.com>
2021-07-18 15:43:13 +0545

    categories list

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
+import CategoryList from "../components/category/CategoryList";
 import BestSellers from "../components/home/BestSellers";
 import NewArrivals from "../components/home/NewArrivals";
 
@@ -20,6 +21,11 @@ const Home = () => {
         Best Sellers
       </h4>
       <BestSellers />
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        Categories
+      </h4>
+      <CategoryList />
       <br />
     </>
   );
