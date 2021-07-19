3514ea4607aa80f634bcd255197319aef6e71fd3
thapaRoyal <thaparoyal17@gmail.com>
2021-07-19 19:51:15 +0545

    style fix

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -5,37 +5,39 @@ const Checkout = () => {
     //
   };
   return (
-    <div className="row">
-      <div className="col-md-6">
-        <h4>Delivery Address</h4>
-        <br />
-        <br />
-        text area
-        <button className="btn btn-primary  mt-2" onClick={saveAddressToDb}>
-          Save
-        </button>
-        <hr />
-        <h4>Got Coupons ?</h4>
-        <br />
-        <br />
-        coupon input and apply button
-      </div>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6">
+          <h4>Delivery Address</h4>
+          <br />
+          <br />
+          text area
+          <button className="btn btn-primary  mt-2" onClick={saveAddressToDb}>
+            Save
+          </button>
+          <hr />
+          <h4>Got Coupons ?</h4>
+          <br />
+          <br />
+          coupon input and apply button
+        </div>
 
-      <div className="col-md-6">
-        <h4>Order Summary</h4>
-        <hr />
-        <p>Products X</p>
-        <hr />
-        <p>List of products</p>
-        <hr />
-        <p>Cart total: $x</p>
-        <div className="row">
-          <div className="col-md-6">
-            <button className="btn btn-primary">Place Order</button>
-          </div>
+        <div className="col-md-6">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products X</p>
+          <hr />
+          <p>List of products</p>
+          <hr />
+          <p>Cart total: $x</p>
+          <div className="row">
+            <div className="col-md-6">
+              <button className="btn btn-primary">Place Order</button>
+            </div>
 
-          <div className="col-md-6">
-            <button className="btn btn-primary">Empty Cart</button>
+            <div className="col-md-6">
+              <button className="btn btn-primary">Empty Cart</button>
+            </div>
           </div>
         </div>
       </div>
