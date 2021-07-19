34427a1c9021342df680a93d7f058249a6402ebd
thapaRoyal <thaparoyal17@gmail.com>
2021-07-19 10:36:10 +0545

    sub category search backend

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -244,8 +244,17 @@ const handleStar = (req, res, stars) => {
     });
 };
 
+const handleSub = async (req, res, sub) => {
+  const products = await Product.find({ subs: sub })
+    .populate("category", "_id name")
+    .populate("subs", "_id name")
+    .populate("postedBy", "_id name")
+    .exec();
+  res.json(products);
+};
+
 exports.searchFilters = async (req, res) => {
-  const { query, price, category, stars } = req.body;
+  const { query, price, category, stars, sub } = req.body;
 
   if (query) {
     console.log("QUERY --->", query);
@@ -265,5 +274,10 @@ exports.searchFilters = async (req, res) => {
   if (stars) {
     console.log("STARS --->", stars);
     await handleStar(req, res, stars);
+  }
+
+  if (sub) {
+    console.log("SUB --->", sub);
+    await handleSub(req, res, sub);
   }
 };
