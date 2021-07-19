b889e46be4534f38808311750136dc4f0a513e78
thapaRoyal <thaparoyal17@gmail.com>
2021-07-19 15:39:28 +0545

    cart page order summary

--- client/src/pages/Cart.js
+++ client/src/pages/Cart.js
@@ -7,11 +7,52 @@ const Cart = () => {
   const { cart, user } = useSelector((state) => ({ ...state }));
   const dispatch = useDispatch();
 
+  //   get total price
+  const getTotal = () => {
+    return cart.reduce((currentValue, nextValue) => {
+      return currentValue + nextValue.count * nextValue.price;
+    }, 0);
+  };
+
   return (
-    <div className="container-fluid">
+    <div className="container-fluid pt-2">
       <div className="row">
-        <h4>Cart</h4>
-        {JSON.stringify(cart)}
+        <h4>Cart / {cart.length} Product</h4>
+      </div>
+      <div className="row">
+        <div className="col-md-8">
+          {!cart.length ? (
+            <p>
+              No products in cart. <Link to="/shop">Continuw shopping</Link>
+            </p>
+          ) : (
+            "show cart items"
+          )}
+        </div>
+        <div className="col-md-4">
+          <h4>Order Summary</h4>
+          <hr />
+          <p>Products</p>
+          {cart.map((c, index) => (
+            <div key={index}>
+              <p>
+                {c.title} X {c.count} =$ {c.price * c.count}
+              </p>
+            </div>
+          ))}
+          <hr />
+          Total: <b>$ {getTotal()}</b>
+          <hr />
+          {user ? (
+            <button className="btn btn-sm btn-primary mt-2">
+              Proceed to checkout
+            </button>
+          ) : (
+            <button className="btn btn-sm btn-primary mt-2">
+              Login to checkout
+            </button>
+          )}
+        </div>
       </div>
     </div>
   );
