ff3ed026624ab548542ba79f900a45129fba153f
thapaRoyal <thaparoyal17@gmail.com>
2021-07-19 22:22:43 +0545

    Final Order Summary in Checkout

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -43,14 +43,21 @@ const Checkout = () => {
 
       <div className="col-md-6">
         <h4>Order Summary</h4>
-        <h1>{total}</h1>
-        {JSON.stringify(products)}
         <hr />
-        <p>Products x</p>
+        <p>Products {products.length}</p>
         <hr />
-        <p>List of products</p>
+        <p>
+          {products.map((p, i) => (
+            <div key={i}>
+              <p>
+                {p.product.title} ({p.color}) X {p.count} =
+                {p.product.price * p.count}
+              </p>
+            </div>
+          ))}
+        </p>
         <hr />
-        <p>Cart Total: $x</p>
+        <p>Cart Total: $ {total}</p>
 
         <div className="row">
           <div className="col-md-6">
