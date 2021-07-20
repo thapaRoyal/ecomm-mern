ca0ec50e1ca17029cbfef681d7cd1aba9d79d30b
thapaRoyal <thaparoyal17@gmail.com>
2021-07-20 22:10:18 +0545

    Showing Each Order's Products in Table

--- client/src/pages/user/History.js
+++ client/src/pages/user/History.js
@@ -14,12 +14,51 @@ const History = () => {
   }, []);
 
   const loadUserOrders = () =>
-    getUserOrders(user.token).then((res) => {
-      console.log(JSON.stringify(res.data, null, 4));
-      setOrders(res.data);
-    });
+    getUserOrders(user.token)
+      .then((res) => {
+        console.log(JSON.stringify(res.data, null, 4));
+        // console.log(res.data[0].products[0].product.title);
+        console.log(res.data.products);
+        setOrders(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
 
-  const showOrderInTable = (order) => <p>Each order and its product</p>;
+  const showOrderInTable = (order) => (
+    <table className="table  table-bordered">
+      <thead className="thead-light">
+        <tr>
+          <th scope="col">Title</th>
+          <th scope="col">Price</th>
+          <th scope="col">Brand</th>
+          <th scope="col">Color</th>
+          <th scope="col">Count</th>
+          <th scope="col">Shipping</th>
+        </tr>
+      </thead>
+      <tbody>
+        {order.products.map((p, i) => (
+          <tr key={i}>
+            <td>
+              <b>{p.product.title}</b>
+            </td>
+            <td>{p.product.price}</td>
+            <td>{p.product.brand}</td>
+            <td>{p.product.color}</td>
+            <td>{p.count}</td>
+            <td>
+              {p.product.shipping === "Yes" ? (
+                <CheckCircleOutlined style={{ color: "green" }} />
+              ) : (
+                <CloseCircleOutlined style={{ color: "red" }} />
+              )}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 
   const showEachOrders = () =>
     orders.map((order, i) => (
