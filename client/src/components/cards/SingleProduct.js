ea4cda839faf8d3b7398a00991b9dd50ea49dd94
thapaRoyal <thaparoyal17@gmail.com>
2021-07-20 20:40:33 +0545

    show out of stock

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -95,10 +95,10 @@ const SingleProduct = ({ product, onStarClick, star }) => {
         <Card
           actions={[
             <Tooltip title={tooltip}>
-              <a onClick={handleAddToCart}>
+              <a onClick={handleAddToCart} disabled={product.quantity < 1}>
                 <ShoppingCartOutlined className="text-danger" />
                 <br />
-                Add to Cart
+                {product.quantity < 1 ? "Out of stock" : "Add to cart"}
               </a>
             </Tooltip>,
 
