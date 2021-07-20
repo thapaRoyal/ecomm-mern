716c7570c1bd58e3a35100d614a46ddd041a26cd
thapaRoyal <thaparoyal17@gmail.com>
2021-07-20 20:16:46 +0545

    Decrement Quantity Increment Sold

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -118,7 +118,10 @@ exports.applyCouponToUserCart = async (req, res) => {
 };
 
 exports.createOrder = async (req, res) => {
+  // console.log(req.body);
+  // return;
   const { paymentIntent } = req.body.stripeResponse;
+
   const user = await User.findOne({ email: req.user.email }).exec();
 
   let { products } = await Cart.findOne({ orderdBy: user._id }).exec();
@@ -128,6 +131,20 @@ exports.createOrder = async (req, res) => {
     paymentIntent,
     orderdBy: user._id,
   }).save();
+
+  // decrement quantity, increment sold
+  let bulkOption = products.map((item) => {
+    return {
+      updateOne: {
+        filter: { _id: item.product._id }, // IMPORTANT item.product
+        update: { $inc: { quantity: -item.count, sold: +item.count } },
+      },
+    };
+  });
+
+  let updated = await Product.bulkWrite(bulkOption, {});
+  console.log("PRODUCT QUANTITY-- AND SOLD++", updated);
+
   console.log("NEW ORDER SAVED", newOrder);
   res.json({ ok: true });
 };
