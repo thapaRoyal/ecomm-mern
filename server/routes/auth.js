b8610144e0519051a836a221e29ad899b636b23f
thapaRoyal <thaparoyal17@gmail.com>
2022-06-26 19:50:21 +0545

    fixed firebase

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,15 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
 // middlewares
-const { authCheck, adminCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require('../middlewares/auth');
 
 // controller
-const { createOrUpdateUser, currentUser } = require("../controllers/auth");
+const { createOrUpdateUser, currentUser } = require('../controllers/auth');
 
-router.post("/create-or-update-user", authCheck, createOrUpdateUser);
-router.post("/current-user", authCheck, currentUser);
-router.post("/current-admin", authCheck, adminCheck, currentUser);
+router.post('/create-or-update-user', authCheck, createOrUpdateUser);
+router.post('/current-user', authCheck, currentUser);
+router.post('/current-admin', authCheck, adminCheck, currentUser);
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,27 +1,36 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const { readdirSync } = require("fs");
-require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const { readdirSync } = require('fs');
+require('dotenv').config();
 
 // app
 const app = express();
 
 // db
-mongoose
-  .connect(process.env.DATABASE, {})
-  .then(() => console.log("DB CONNECTED"))
-  .catch((err) => console.log("DB CONNECTION ERR", err));
+// import statements
+// Database connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('DB connected...');
+});
 
 // middlewares
-app.use(morgan("dev"));
-app.use(bodyParser.json({ limit: "2mb" }));
+app.use(morgan('dev'));
+app.use(bodyParser.json({ limit: '2mb' }));
 app.use(cors());
 
 // routes middleware
-readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
 
 // port
 const port = process.env.PORT || 8000;
